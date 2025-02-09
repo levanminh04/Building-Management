903e1f20b90f5b740b8cc341eb87d325c53f1a3d
levanminh04 <levanminh031204@gmail.com>
2025-02-09 19:24:06 +0700

    update

--- src/main/java/com/javaweb/config/WebConfig.java
+++ src/main/java/com/javaweb/config/WebConfig.java
@@ -11,8 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**", "/js/**", "/images/**", "/fonts/**", "/login/**", "/admin/**", "/web/**")
-                .addResourceLocations("classpath:/static/css/", "classpath:/static/js/", "classpath:/static/images/", "classpath:/static/fonts/", "classpath:/static/login/", "classpath:/static/admin/")
-                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic());
+        registry.addResourceHandler("/**") // Áp dụng cho tất cả file tĩnh
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic()); // Cho phép cache
     }
 }
--- src/main/java/com/javaweb/config/WebSecurityConfig.java
+++ src/main/java/com/javaweb/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -72,7 +73,7 @@ public class WebSecurityConfig {
                             .anyRequest().authenticated();
                 })
                 .headers(headers -> headers
-                        .cacheControl(cache -> cache.disable()) // ✅ Cho phép cache
+                        .cacheControl(HeadersConfigurer.CacheControlConfig::disable) // ✅ Cho phép cache
                 )
                 .logout(logout -> {
                     logout
